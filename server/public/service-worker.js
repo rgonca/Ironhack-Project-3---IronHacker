123d1e87d9f7a2de46b02bf205bacc76c8ed82d8
Roberto Gonzalez <rob.gonzalez3@gmail.com>
2020-07-30 13:39:29 +0200

    deploy

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7f7b1dd0cfec196cfa51651eb3240d36.js"
+  "/precache-manifest.6cb955502eb71d1b3006e53661f6fd7b.js"
 );
 
 self.addEventListener('message', (event) => {
